b219eff27703c0a2d5130b45c0f5a0b31c8d36a5
Malathicse <malathi141996@gmail.com>
2016-07-21 09:37:21 +0530

    Create stringrev.java

---
+++ stringrev.java
@@ -0,0 +1,9 @@
+public class stringrev{
+public static void main(String[] args){
+String str="reverse";
+int len=str.length();
+for(int i=len-1;i>=0;i++){
+System.out.println(str.charAt(i);
+}
+}
+}
